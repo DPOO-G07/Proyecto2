60ed8bee80794d8419136024f71d1caa6c68634f
Samara-MJ <s.martinezj@uniandes.edu.co>
2023-12-08 11:43:22 -0500

    testcarga

--- src/tests/PruebaCargadeArchivos.java
+++ src/tests/PruebaCargadeArchivos.java
@@ -1,14 +1,32 @@
 package tests;
+import presentacion.carga;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import Modelo.Rentadora;
+
 class PruebaCargadeArchivos {
+	
+	@BeforeEach
+	public void setUp() throws Exception {
+		 new carga();
+	}
+
+	
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	public void carga() {
+		Rentadora rentadora = carga.Leer("data/personas.txt", "data/sede.txt", "data/reserva.txt", "data/vehiculos.txt","data/Proveedores.txt","data/Seguros.txt","data/MetododePago.txt");	
+		assertNotNull(rentadora);
 	}
+	@Test
+	public void carga2() {
+		Rentadora rentadora = carga.Leer("data/persona.txt", "data/sede.txt", "data/reserva.txt", "data/vehiculos.txt","data/Proveedores.txt","data/Seguros.txt","data/MetododePago.txt");	
+		assertNull(rentadora);
+	}
+
 
 }
